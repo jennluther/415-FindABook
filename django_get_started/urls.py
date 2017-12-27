da16dec0c7f84c0c0e5d6177236a623ef7e7c301
jennluther <luther.jenn@gmail.com>
2017-12-26 20:49:37 -0700

    urls again

--- django_get_started/urls.py
+++ django_get_started/urls.py
@@ -1,29 +1,29 @@
 """
-Definition of urls for DjangoApp.
+Definition of urls for django_get_started.
 """
 
 from datetime import datetime
-from django.conf.urls import url
+from django.conf.urls import patterns, url
 from app.forms import BootstrapAuthenticationForm
 from app.views import *
 from app.models import *
 from django.contrib.auth.views import *
-
 
 # Uncomment the next lines to enable the admin:
 # from django.conf.urls import include
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = [
+urlpatterns = patterns('',
     # Examples:
-
-    url(r'^$', home, name='home'),
-    url(r'^contact$', contact, name='contact'),
-    url(r'^about', about, name='about'),
-    url(r'^dashboard', dashboard, name='dashboard'),
-    url(r'^recommender', recommender, name='recommender'),
-   url(r'^login/$', login, {
+    url(r'^$', 'app.views.home', name='home'),
+    url(r'^contact$', 'app.views.contact', name='contact'),
+    url(r'^about', 'app.views.about', name='about'),
+    url(r'^dashboard', 'app.views.dashboard', name='dashboard'),
+    url(r'^recommender', 'app.views.recommender', name='recommender'),
+    url(r'^login/$',
+        'django.contrib.auth.views.login',
+        {
             'template_name': 'app/login.html',
             'authentication_form': BootstrapAuthenticationForm,
             'extra_context':
@@ -33,11 +33,16 @@ urlpatterns = [
             }
         },
         name='login'),
-       url(r'^logout$', logout, {  'next_page': '/'  },        name='logout')
+    url(r'^logout$',
+        'django.contrib.auth.views.logout',
+        {
+            'next_page': '/',
+        },
+        name='logout'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-]
+)
