e574d95a30a2926903a65d72c494275743aeb079
jennluther <luther.jenn@gmail.com>
2017-12-27 10:22:42 -0700

    change back

--- app/models.py
+++ app/models.py
@@ -18,9 +18,6 @@ class Books(models.Model):
     ratings_4 = models.IntegerField()
     ratings_5 = models.IntegerField()
 
-    class Meta:
-        ordering = ('title',)
-
 
     def __unicode__(self):
         return self.title + " by " + self.authors
